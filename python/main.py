79462d200dba7d015ac7a92ebfeff9a3cc525150
oyberntzen <oyberntzen@gmail.com>
2024-04-02 21:42:11 +0200

    Added plot choices

--- python/main.py
+++ python/main.py
@@ -9,12 +9,14 @@ def main():
     data_manager = data.DataManager(None)
 
     variables = list(plot.all_variables().values())
-    term = tui.TUI(data_manager.all_sessions(), variables, 2)
+    term = tui.TUI(data_manager.all_sessions(), variables)
 
     current_session = None
     plot_pipes = {}
 
     while True:
+        term_changed = False
+
         new_data = data_manager.read_serial_packet()
         if new_data != None:
             term.change_sessions(data_manager.all_sessions())
@@ -31,14 +33,13 @@ def main():
                     plot_pipes[session].remove(pipe)
 
             current_packets = []
-            if current_session != None:
-                current_packets = data_manager.sessions[current_session]
-            term.draw(current_packets)
+
+            term_changed = True
 
         current_packets = []
         if current_session != None:
             current_packets = data_manager.sessions[current_session]
-        message = term.update(current_packets)
+        message = term.update()
         if message != None:
             flag, message_data = message
             if flag == tui.QUIT:
@@ -54,6 +55,14 @@ def main():
                 plotter = plot.Plot2D(plot_pipe, message_data[0].name, message_data[1].name, data_manager.sessions[current_session])
                 plot_process = multiprocessing.Process(target=plotter.run)
                 plot_process.start()
+
+            if message != None:
+                term_changed = True
+        
+        if term_changed:
+            if current_session != None:
+                current_packets = data_manager.sessions[current_session]
+            term.draw(current_packets)
 
         time.sleep(1/60)
 
--- python/tui.py
+++ python/tui.py
@@ -29,22 +29,64 @@ class Option:
         self.selected = new_selected
         self.options = new_options
 
+class ChoiceField:
+    def __init__(self, choices):
+        self.choices = choices
+        self.selected = 0
+
+    def __str__(self):
+        return str(self.choices[self.selected])
+
+    def update(self, key):
+        if key == "f":
+            self.selected = (self.selected+1) % len(self.choices)
+            return True
+        return False
+
+class NumberField:
+    def __init__(self):
+        self.num = 0
+
+    def __str__(self):
+        return str(self.num)
+
+    def update(self, key):
+        if key == "f":
+            self.num = 0
+            return True
+        if key.isdigit():
+            digit = int(key)
+            self.num *= 10
+            self.num += digit
+            return True
+        return False
+
 QUIT = 0
 CHANGE_SESSION = 1
 PLOT_VARIABLE = 2
+CHANGED = 3
 
 class TUI:
-    def __init__(self, sessions, variables, dimensions):
+    def __init__(self, sessions, variables):
         self.packet_pos = (0, 1)
-        self.sessions_pos = (40, 1)
+        self.sessions_pos = (30, 1)
+        self.plot_pos = (60, 1)
         self.options_pos = (80, 1)
         self.options_spacing = 15
 
-        self.dimensions = dimensions
+        self.session_option = Option(sessions, self.sessions_pos[0], self.sessions_pos[1]) 
 
-        self.options = [Option(sessions, self.sessions_pos[0], self.sessions_pos[1])]
-        for i in range(self.dimensions):
-            self.options.append(Option(variables, self.options_pos[0]+self.options_spacing*i, self.options_pos[1]))
+        self.num_dimensions = ChoiceField(["2D", "3D"])
+        self.plot_start_choice = ChoiceField(["From:", "Last:"])
+        self.plot_start_num = NumberField()
+        self.plot_option = Option([self.num_dimensions, self.plot_start_choice, self.plot_start_num], self.plot_pos[0], self.plot_pos[1])
+
+        self.dimensions_option = []
+        max_dimensions = 3
+        for i in range(max_dimensions):
+            self.dimensions_option.append(Option(variables, self.options_pos[0]+self.options_spacing*i, self.options_pos[1]))
+
+        self.options = [self.session_option] + [self.plot_option] + self.dimensions_option
         self.current_option = 0
 
         self.variables = variables
@@ -52,7 +94,7 @@ class TUI:
         ptg.hide_cursor()
         self.draw([])
 
-    def update(self, packets):
+    def update(self):
         key = ptg.getch()
         message = None
         changed = True
@@ -68,20 +110,28 @@ class TUI:
             self.current_option = max(self.current_option-1, 0)
         elif key == "d":
             self.current_option += 1
+
             if self.current_option == 1:
                 new_session = self.options[0].selected_option()
                 message = (CHANGE_SESSION, new_session)
-            elif self.current_option == len(self.options):
+
+            num_dimensions = self.num_dimensions.selected+2
+            if self.current_option == num_dimensions+2:
                 self.current_option = 1
                 variables = []
-                for i in range(self.dimensions):
-                    variables.append(self.options[i+1].selected_option())
+                for i in range(num_dimensions):
+                    variables.append(self.dimensions_option[i].selected_option())
                 message = (PLOT_VARIABLE, variables)
-        else:
-            changed = False
 
-        if changed:
-            self.draw(packets)
+        else:
+            selected = self.options[self.current_option].selected_option()
+            if hasattr(selected, "update"):
+                changed = selected.update(key)
+            else:
+                changed = False
+
+        if changed and message == None:
+            message = (CHANGED, None)
 
         return message
 
@@ -94,6 +144,8 @@ class TUI:
         if len(packets) > 0:
             for i, variable in enumerate(self.variables):
                 value = variable.value(packets)
+                if type(value) == float:
+                    value = round(value, 4)
                 ptg.print_to((self.packet_pos[0], self.packet_pos[1]+i), f"{variable.name}: {value}")
 
     def change_sessions(self, new_sessions):
